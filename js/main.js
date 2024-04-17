0c619c154ef2543d0e8e994d42f89c4a8d1e2487
Saheer Babu <sheerbabu549@gmail.com>
2024-04-17 23:27:26 +0530

    recreated

--- js/main.js
+++ js/main.js
@@ -68,23 +68,74 @@ if (userInput != null) {
   if(userInput==1){
     postRef.update(update1).then(() => {
         console.log('Update successful!');
+
+        uploadBtn.classList.remove("btn_loading")
+                uploadBtn.innerHTML=`<img class="doneGif" src="./done.gif" alt="">`
+                uploadBtn.style.border="solid 1px #41B06E"
+                uploadBtn.style.background="#222831"
+                setTimeout(success, 3000)
+
+                
       }).catch((error) => {
         console.error('Update failed: ' + error.message);
+
+        uploadBtn.style.border="solid 1px red"
+            uploadBtn.innerHTML=`
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
+            `
+            setTimeout(success, 3000)
       });
       
   } else {
     if (userInput==2) {
         postRef.update(update2).then(() => {
             console.log('Update successful!');
+
+            uploadBtn.classList.remove("btn_loading")
+                uploadBtn.innerHTML=`<img class="doneGif" src="./done.gif" alt="">`
+                uploadBtn.style.border="solid 1px #41B06E"
+                uploadBtn.style.background="#222831"
+                setTimeout(success, 3000)
+
+
           }).catch((error) => {
             console.error('Update failed: ' + error.message);
+
+            uploadBtn.style.border="solid 1px red"
+            uploadBtn.innerHTML=`
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
+            `
+            setTimeout(success, 3000)
           });
     } else {
         if (userInput == 3) {
             postRef.update(update3).then(() => {
                 console.log('Update successful!');
+
+                uploadBtn.classList.remove("btn_loading")
+                uploadBtn.innerHTML=`<img class="doneGif" src="./done.gif" alt="">`
+                uploadBtn.style.border="solid 1px #41B06E"
+                uploadBtn.style.background="#222831"
+                setTimeout(success, 3000)
+
+
               }).catch((error) => {
-                console.error('Update failed: ' + error.message);
+                alert('Update failed: ' + error.message);
+
+                uploadBtn.style.border="solid 1px red"
+            uploadBtn.innerHTML=`
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
+            `
+            setTimeout(success, 3000)
               });
         }
     }
